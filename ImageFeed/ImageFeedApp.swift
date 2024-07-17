41dcf0676867946e077717e6a6288a88aa88d6dd
Nikita Bohatyrov <nikitka_bg15@ukr.net>
2024-07-18 02:12:23 +0300

    Initial Commit

---
+++ ImageFeed/ImageFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageFeedApp.swift
+//  ImageFeed
+//
+//  Created by никита богатырев on 18.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct ImageFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
